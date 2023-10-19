cc2b4f6e838b65ebc75812b6e8525c89c66ae59a
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-10-19 08:48:37 +0700

    second commit

--- app/src/main/java/com/example/actionbar/MainActivity.kt
+++ app/src/main/java/com/example/actionbar/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.actionbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_home -> {
+                Toast.makeText(this, "Halaman Home", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_quiz -> {
+                Toast.makeText(this, "Halaman Quiz", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_assignment -> {
+                Toast.makeText(this, "Halaman Assignment", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
